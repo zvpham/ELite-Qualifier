dadcba49f0be398c55eab38b55e6697f421fb4b0
zvpham <90424742+zvpham@users.noreply.github.com>
2021-10-26 19:24:57 +0000

    added comments

--- main.py
+++ main.py
@@ -15,78 +15,90 @@ def load_words():
 
   return all_words
 
-def check_errors(text, word):
-  error  = 0
-  incorrect_letter = 0
-  #print(word)
-  for i in range(len(text)):
-    if(text[i] != word[i]):
-      incorrect_letter += 1
-      if(text[i] !=  word[i - 1] and text[i] != word[i + 1]):
-        error += 1
-  #print(error)
-  return [error, incorrect_letter]
+"""
+Parameters: user_word - the word that the user inputed,
+            word -  a word that the code believes could be similar to user_word
 
+Function: Compares the User's word to a word in the list possible_words and returns an integer of how many letters did not match within a range of one space between both words  
+"""
+def check_errors(user_word, word):
+  error  = 0
+  for i in range(len(user_word)):
+    if(user_word[i] != word[i]):
+      if(user_word[i] !=  word[i - 1] and user_word[i] != word[i + 1]):
+        error += 1
+  return error
+
+"""
+Parameters: word_list - a list of all possible words 
+            words_shown_to_user - list of words that were shown to user to likely be the one they meant to type
+
+Function: asks the user if they want to remove a word from word_list and if yes choose which words out of words_shown_to_user to remove from it for the rest of the programs runtime
+"""
 def ignore_words( word_list, words_shown_to_user):
-  # words_to_ignore = []
-  # print(words_shown_to_user)
-  # print(word_list)
   ignore = input("do you want to ignore any words on the list (yes) or (no) ")
   while (ignore ==  "yes"):
     ignore_word = int(input("type in the number of the word "))
     word_list.remove(words_shown_to_user[ignore_word])
-
+    print("")
     ignore =  input("are there any more words you want to ignore (yes) or (no) ")
 
 
+"""
+Parameters: all_words - a list of all possible words 
+            user_word - a word that the user inputed
 
-def suggest(text, all_words):
+Function: The user inputs a word and if it isn't a word in safedict_simple suggest a word that could be the word that the user inteded to type
+"""
+def suggest(user_word, all_words):
   # YOUR CODE HERE. This currently doesn't suggest a correction, just checks if the input is already a word. You'll want to change that
 
-  possible_words = [
+  possible_words = []
+  possible_correct_words = [ ]
 
-  ]
-
-  possible_correct_words = [ 
-    
-  ]
-
-  if text in all_words:
-    print(text + ' is a word')
+  # checks to see if the word the user inputed was a word found in safedict_simple and if no create a list of possible words that they meant to type
+  if user_word in all_words:
+    print(user_word + ' is a word')
   else:
    for word in all_words:
-     if(word[0] ==  text[0] and (len(word) >= len(text) - 1) and len(word) <= len(text) + 1):
+     if(word[0] ==  user_word[0] and (len(word) >= len(user_word) - 1) and len(word) <= len(user_word) + 1):
        possible_words.append(word)
 
+  # finds amount of or letters that aren't the same within a range of one place between the word the user inputed and a possible word
   for word in possible_words:
-   # print(f" first instance of possible word - {word}")
     temp = word 
     errors =  0
 
-    if(len(word) != len(text)):
-      if(len(text) > len(word)):
+    if(len(word) != len(user_word)):
+      if(len(user_word) > len(word)):
         temp += "**"
-        errors = check_errors(text, temp)
+        errors = check_errors(user_word, temp)
       else:
-        errors = check_errors(text,temp)
+        errors = check_errors(user_word,temp)
     else:
       temp += "*"
-      errors = check_errors(text,temp)
+      errors = check_errors(user_word,temp)
 
-    if(errors[0] <= 1):
+    if(errors <= 1):
       possible_correct_words.append(word)
-    
-  #print(possible_correct_words)
+
+  if len(possible_correct_words) == 0:
+    print(f"{user_word} isn't a word")
+    return ""
+
   for i in range(len(possible_correct_words)):
     print(f"did you mean [{i}] {possible_correct_words[i]}")
+  print("")
+
   
   if(len(possible_correct_words) > 0):
     choice =  int(input(" type in the number of the word you mean to type "))
     real_word = possible_correct_words[choice]
+    print("")
+    ignore_words(all_words ,possible_correct_words)
   else:
-    real_word = text
+    real_word = user_word
 
-  ignore_words(all_words ,possible_correct_words)
   real_word += " "
   return real_word
 
@@ -95,11 +107,12 @@ def main():
     sentence = ""
     while True:
         print('Type some text, or type \"quit\" to stop')
-        text = input(':> ')
-        if ('quit' == text):
+        user_word = input(':> ')
+        if ('quit' == user_word):
           print(sentence)
           break
-        sentence += suggest(text, all_words)
+        sentence += suggest(user_word, all_words)
+        print(f"sentence so far: {sentence}")
 
         
 
